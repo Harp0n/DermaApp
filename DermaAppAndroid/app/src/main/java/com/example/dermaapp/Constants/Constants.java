fd6636bbb491364d1644b031713999b3d9223fd3
Radosław Karbowiak <41492536+Reevo55@users.noreply.github.com>
2020-02-06 23:47:35 +0100

    Check

--- DermaAppAndroid/app/src/main/java/com/example/dermaapp/Constants/Constants.java
+++ DermaAppAndroid/app/src/main/java/com/example/dermaapp/Constants/Constants.java
@@ -1,15 +1,7 @@
 package com.example.dermaapp.Constants;
 
-import android.Manifest;
 import android.graphics.Color;
 
 public class Constants {
     public static int BACKGROUND_COLOR = Color.BLUE;
-    public static final int GALLERY_REQUEST_CODE = 1;
-    public static final int PHOTO_REQUEST_CODE = 0;
-    public static final int REQUEST_EXTERNAL_STORAGE = 1;
-    public static final String[] cameraPermissions ={
-            Manifest.permission.CAMERA,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-    };
 }
