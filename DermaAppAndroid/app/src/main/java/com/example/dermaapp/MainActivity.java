30df34f0f1af9e529f592cd16d47fdb4f4acb7cd
PitiMonster <piotrszym159@gmail.com>
2020-02-06 23:46:08 +0100

    photo by camera or from gallery

--- DermaAppAndroid/app/src/main/java/com/example/dermaapp/MainActivity.java
+++ DermaAppAndroid/app/src/main/java/com/example/dermaapp/MainActivity.java
@@ -1,13 +1,78 @@
 package com.example.dermaapp;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Parcelable;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.media.MediaRecorder.VideoSource.CAMERA;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonGallery;
+    private Button buttonPhoto;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonGallery = findViewById(R.id.buttonGalleryPicture);
+        buttonPhoto = findViewById(R.id.buttonCameraPhoto);
+        imageView = findViewById(R.id.imageView);
+
+        buttonGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(pickPhoto, 1);//one can be replaced with any action code
+            }
+        });
+
+        buttonPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(takePicture, 0);//zero can be replaced with any action code (called requestCode)
+            }
+        });
+
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch (requestCode) {
+            case 0:
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    File file = new File(selectedImage.getPath());
+                    imageView.setImageURI(selectedImage);
+                }
+
+                break;
+        }
     }
 }
+
