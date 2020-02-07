396375ce245265b10575f4ebf5eb6b9446902db5
Radosław Karbowiak <41492536+Reevo55@users.noreply.github.com>
2020-02-07 22:11:12 +0100

    Problem with sending POST request

--- DermaAppAndroid/app/src/main/java/com/example/dermaapp/Controler/IDjangoApi.java
+++ DermaAppAndroid/app/src/main/java/com/example/dermaapp/Controler/IDjangoApi.java
@@ -9,9 +9,8 @@ import retrofit2.http.POST;
 import retrofit2.http.Part;
 
 public interface IDjangoApi {
+
     String DJANGO_SITE = "http://192.168.1.21:8000/analyze/";
-
-
 
     @Multipart
     @POST("upload/")
--- DermaAppAndroid/app/src/main/java/com/example/dermaapp/MainActivity.java
+++ DermaAppAndroid/app/src/main/java/com/example/dermaapp/MainActivity.java
@@ -1,27 +1,70 @@
 package com.example.dermaapp;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Parcelable;
 import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.example.dermaapp.Classes.PathUtil;
+import com.example.dermaapp.Controler.IDjangoApi;
+import com.example.dermaapp.Controler.ServerControler;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import android.util.Log;
+
+import java.io.File;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import static android.media.MediaRecorder.VideoSource.CAMERA;
 
@@ -30,6 +73,8 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonGallery;
     private Button buttonPhoto;
     private ImageView imageView;
+    private static final int CAMERA_REQUEST = 1888;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,39 +85,102 @@ public class MainActivity extends AppCompatActivity {
         buttonPhoto = findViewById(R.id.buttonCameraPhoto);
         imageView = findViewById(R.id.imageView);
 
-        buttonGallery.setOnClickListener(new View.OnClickListener() {
+        OkHttpClient client = new OkHttpClient();
+        String url = "http://192.168.1.21:8000/analyze/image";
+        Request request = new Request.Builder().url(url).build();
+        client.newCall(request).enqueue(new okhttp3.Callback() {
             @Override
-            public void onClick(View v) {
-                Intent pickPhoto = new Intent(Intent.ACTION_PICK,
-                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(pickPhoto, 1);//one can be replaced with any action code
+            public void onFailure(okhttp3.Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(okhttp3.Call call, okhttp3.Response response) throws IOException {
+
             }
         });
 
-        buttonPhoto.setOnClickListener(new View.OnClickListener() {
+        this.imageView = (ImageView)this.findViewById(R.id.imageView);
+        buttonPhoto.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(takePicture, 0);//zero can be replaced with any action code (called requestCode)
+            public void onClick(View v)
+            {
+                if (false)
+                {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+                }
+                else
+                {
+                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                }
             }
         });
-
 
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
-        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
-        switch (requestCode) {
-            case 0:
-            case 1:
-                if (resultCode == RESULT_OK) {
-                    Uri selectedImage = imageReturnedIntent.getData();
-                    File file = new File(selectedImage.getPath());
-                    imageView.setImageURI(selectedImage);
-                }
-
-                break;
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_PERMISSION_CODE)
+        {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+            else
+            {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+            }
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK)
+        {
+            try {
+                Bitmap photo = (Bitmap) data.getExtras().get("data");
+                //create a file to write bitmap data
+                File f = new File(getApplicationContext().getCacheDir(), "photo");
+                f.createNewFile();
+
+                //Convert bitmap to byte array
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                photo.compress(Bitmap.CompressFormat.PNG, 0 /*ignored for PNG*/, bos);
+                byte[] bitmapdata = bos.toByteArray();
+
+                //write the bytes in file
+                FileOutputStream fos = new FileOutputStream(f);
+                fos.write(bitmapdata);
+                fos.flush();
+                fos.close();
+
+                ServerControler serverControler = new ServerControler();
+                serverControler.uploadFoto(getApplicationContext(), f);
+
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getPath(Uri uri)
+    {
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+        if (cursor == null) return null;
+        int column_index =             cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String s=cursor.getString(column_index);
+        cursor.close();
+        return s;
     }
 }
 
