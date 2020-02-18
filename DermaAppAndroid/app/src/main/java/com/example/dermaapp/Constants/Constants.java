ea0920b972c78a0b740c8719f53702116ac043f6
PitiMonster <piotrszym159@gmail.com>
2020-02-18 12:08:44 +0100

    gui update

--- DermaAppAndroid/app/src/main/java/com/example/dermaapp/Constants/Constants.java
+++ DermaAppAndroid/app/src/main/java/com/example/dermaapp/Constants/Constants.java
@@ -1,5 +1,6 @@
 package com.example.dermaapp.Constants;
 
+import android.Manifest;
 import android.graphics.Color;
 
 public class Constants {
@@ -7,4 +8,8 @@ public class Constants {
     public static final int GALLERY_REQUEST_CODE = 1;
     public static final int PHOTO_REQUEST_CODE = 0;
     public static final int REQUEST_EXTERNAL_STORAGE = 1;
+    public static final String[] cameraPermissions ={
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 }
