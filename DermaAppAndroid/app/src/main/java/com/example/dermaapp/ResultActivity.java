77b31d42527a0807ca2c1c07e21b5327a34e11b2
PitiMonster <piotrszym159@gmail.com>
2020-03-03 23:19:22 +0100

    git troubles

--- DermaAppAndroid/app/src/main/java/com/example/dermaapp/ResultActivity.java
+++ DermaAppAndroid/app/src/main/java/com/example/dermaapp/ResultActivity.java
@@ -1,9 +1,11 @@
 package com.example.dermaapp;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import static android.support.v4.graphics.ColorUtils.HSLToColor;
 
 import com.example.dermaapp.Controler.ServerResponse;
 
@@ -19,7 +21,11 @@ public class ResultActivity extends AppCompatActivity {
         TextView textViewResult = findViewById(R.id.textViewResult);
 
         progressBar.setMax(100);
-        progressBar.setProgress(0);
+        //progressBar.setProgress(0);
+
+        ObjectAnimator.ofInt(progressBar, "progress", 100)
+                .setDuration(1000)
+                .start();
 
         ServerResponse serverResponse = new ServerResponse();
         serverResponse.getTextViewsToUpdate().add(textViewResult);
